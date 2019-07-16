1a66f759d0c83edc2383d314c0d99a21115039ec
Eric Pedersen <epeder10@gmail.com>
2019-07-16 18:11:03 -0400

    INIT - Adding initial reducers and actions

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, StatusBar } from 'react-native';
+import { purple } from './utils/colors'
+import { Constants } from 'expo'
+
+function UdaciStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
+      <Text>Select a c</Text>
+      <FlatList
+        data={[{key: 'a'}, {key: 'b'}]}
+        renderItem={({item}) => <Text>{item.key}</Text>}
+      />
     </View>
   );
 }
